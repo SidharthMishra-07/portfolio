9baae0b98c98570bc12febfa8f1cc5ec615e9e53
Sidharth Mishra <sidharth1604@gmail.com>
2022-12-31 02:54:49 +0530

    Footer Done

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -2,29 +2,45 @@ import React from 'react';
 import './Footer.css';
 
 const Footer = () => {
-  return (
-    <footer className="footer">
-        <div className="footer_container container">
-            <h1 className="footer_title">
-                Sidharth Mishra
-            </h1>
-            <ul className='footer_list'>
-                <li>
-                    <a href="#about" className="footer_link">About</a>
-                </li>
-                <li>
-                    <a href="#skills" className="footer_link">Skills</a>
-                </li>
-                <li>
-                    <a href="#projects" className="footer_link">Project</a>
-                </li>
-                <li>
-                    <a href="#contact" className="footer_link">Contact</a>
-                </li>
-            </ul>
-        </div>
-    </footer>
-  )
+    return (
+        <footer className="footer">
+            <div className="footer_container container">
+                <h1 className="footer_title">
+                    Sidharth Mishra
+                </h1>
+                <ul className='footer_list'>
+                    <li>
+                        <a href="#about" className="footer_link">About</a>
+                    </li>
+                    <li>
+                        <a href="#skills" className="footer_link">Skills</a>
+                    </li>
+                    <li>
+                        <a href="#projects" className="footer_link">Project</a>
+                    </li>
+                    <li>
+                        <a href="#contact" className="footer_link">Contact</a>
+                    </li>
+                </ul>
+
+                <div className="footer_social">
+                    <a href="https://github.com/SidharthMishra-07" className="footer_social-link" target="_blank" rel='noreferrer'>
+                        <i className="bx bxl-github"></i>
+                    </a>
+
+                    <a href="https://www.instagram.com/its_captain_sid/" className="footer_social-link" target="_blank" rel='noreferrer'>
+                        <i className="bx bxl-instagram"></i>
+                    </a>
+
+                    <a href="https://twitter.com/Sid_Mishra07" className="footer_social-link" target="_blank" rel='noreferrer'>
+                        <i className="bx bxl-twitter"></i>
+                    </a>
+                </div>
+
+                <span className="footer_copy">&#169; Sidharth Mishra. All rigths reserved</span>
+            </div>
+        </footer>
+    )
 }
 
 export default Footer
