91518bdc332db442a1637dfa88b29a9729ac44ad
Sidharth Mishra <sidharth1604@gmail.com>
2022-12-30 23:12:16 +0530

    Skills Page Added

---
+++ src/Components/Skills/Skills.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Skills.css';
+
+const Skills = () => {
+  return (
+    <div className="skills section" id="skills">
+        <h2 className="section_title">Skills</h2>
+        <span className="section_subtitle">My technical level</span>
+        <div className="skills_container container grid">
+            
+        </div>
+    </div>
+  )
+}
+
+export default Skills
