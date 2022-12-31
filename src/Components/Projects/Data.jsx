6430a003fdb9ea17625862d558f5a244ff986193
Sidharth Mishra <sidharth1604@gmail.com>
2022-12-31 16:11:26 +0530

    Projects Updated

--- src/Components/Projects/Data.jsx
+++ src/Components/Projects/Data.jsx
@@ -11,24 +11,24 @@ export const projectsData = [
       image: Work1,
       heading: "CodeChefVIT Official Website",
       title: "Web design",
-      category: "web",
-      link:"https://github.com/CodeChefVIT/CodeChefVIT21"
+      link:"https://github.com/CodeChefVIT/CodeChefVIT21",
+      desc:"The official website of CodeChef-VIT. Developed the whole frontend aspect of the official webpage and thoroughly tested it for responsiveness and cross-browser compatibility."
     },
     {
       id: 2,
       image: Work2,
       heading: "CodeInvicta 22",
       title: "Web design",
-      category: "web",
-      link:"https://github.com/CodeChefVIT/CodeInvicta"
+      link:"https://github.com/CodeChefVIT/CodeInvicta",
+      desc:"The official webpage of CodeChef-VIT's major coding contest. Updated the CodeInvicta website and added some new features. Improved the SEO tags and increased 25% more reach"
     },
     {
       id: 3,
       image: Work3,
       heading: "VI-GO",
       title: "Web design",
-      category: "web",
-      link:"https://github.com/SidharthMishra-07/VIGO"
+      link:"https://github.com/SidharthMishra-07/VIGO",
+      desc:"A cycle rental application created for college students. Allows you to rent cycles on an hourly basis from fellow students on the go. You use the cycle only for the time you need it and return it to the owner. This helps you save money and also helps the environment"
     },
     {
       id: 4,
@@ -36,23 +36,24 @@ export const projectsData = [
       heading: "NFT Ticketing System",
       title: "Web design",
       category: "web",
-      link:""
+      link:"",
+      desc:"Developed to eliminate black tickets. A ticketing system for events that uses NFTs to create tickets. The tickets are stored on the blockchain and can be transferred to other users. This ensures that artists get paid for their work and the tickets are not sold at a higher price"
     },
     {
       id: 5,
       image: Work6,
       heading: "Portfolio website",
       title: "Web design",
-      category: "web",
-      link:"https://github.com/SidharthMishra-07/portfolio"
+      link:"https://github.com/SidharthMishra-07/portfolio",
+      desc:"My personal portfolio website. Developed using ReactJS and currently hosted on vercel. The website is fully responsive and is cross-browser compatible."
     },
     {
       id: 6,
       image: Work5,
       heading: "News API website",
       title: "Web design",
-      category: "web",
-      link:"https://github.com/SidharthMishra-07/media"
+      link:"https://github.com/SidharthMishra-07/media",
+      desc:"A news website that uses the News API to fetch news articles. One can search top news on a specific topic and news can be retrieved based on some criteria."
     },
   ];
   
--- src/Components/Projects/Workitem.jsx
+++ src/Components/Projects/Workitem.jsx
@@ -5,9 +5,10 @@ const WorkItem = ({item}) => {
     <div className="work_card" key={item.id}>
         <h2 className="work_title center">{item.heading}</h2>
         <img src={item.image} alt="Work1" className='work_img'/>
-        <h3 className="work_title">{item.title}</h3>
+        <h3 className="work_desc">{item.desc}</h3>
+        {/* <h3 className="work_title">{item.title}</h3> */}
         <a href={item.link} target="_blank" rel='noreferrer' className="work_button">
-            Check it out<i className='bx bx-right-arrow-alt work_button-icon'></i>
+          <i className="bx bxl-github"></i> Check it out <i className='bx bx-right-arrow-alt work_button-icon'></i>
         </a>
     </div>
   )
